4d66406f13234e19cc519fce3f6a0aa078c89df5
alicia <sleechengn@live.cn>
2025-08-10 10:47:30 +0000

    保存程序

---
+++ install-hook.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/bash
+INSTALLPATH=$(pwd)
+
+if [ ! -e "/var/lib/vz/snippets" ]; then
+    mkdir -p /var/lib/vz/snippets
+fi
+
+if [ ! -e "/etc/systemd/system/fifo.service" ]; then
+
+cat > /etc/systemd/system/fifo.service <<EOF
+[Unit]
+Description=FIFO QUEUE
+After=network-online.target
+
+[Service]
+ExecStart=/usr/bin/bash $INSTALLPATH/fifo/queue.sh
+WorkingDirectory=$INSTALLPATH/fifo
+User=root
+Type=oneshot
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+chmod +x $INSTALLPATH/fifo/queue.sh
+systemctl enable fifo.service
+systemctl start fifo.service &
+
+fi
+
+cat > /var/lib/vz/snippets/pvevm-hooks-nv.sh <<EOF
+#!/usr/bin/bash
+$INSTALLPATH/pvevm-hooks/vm-hook.sh \$*
+EOF
+
+chmod +x /var/lib/vz/snippets/pvevm-hooks-nv.sh
+chmod +x $INSTALLPATH/pvevm-hooks/*.sh
+
+if [ "$1" ]; then
+    qm set $1 --hookscript local:snippets/pvevm-hooks-nv.sh
+    else
+    echo "useage: ./install-hook.sh <vmid>"
+fi
